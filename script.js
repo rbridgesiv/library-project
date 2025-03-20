ec6114fcde2b5eb065411f88c348e3c787e45e0e
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-03-20 16:11:54 +0200

    create constructor, addBook function, and function that displays books on screen after load

---
+++ script.js
@@ -0,0 +1,35 @@
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.id = crypto.randomUUID();
+}
+
+const myLibrary = [
+    new Book("The Hobbit", "J.R.R. Tolkein", 1937),
+    new Book("To Kill A Mockingbird", "Harper Lee", 1960),
+    new Book("1984", "George Orwell", 1949)
+];
+
+function addBookToLibrary(title, author, year) {
+    const newBook = new Book(`${this.title} by ${this.author}, ${this.year}`);
+    myLibrary.push(newBook);
+}
+
+function displayBook() {
+    const bookContainer = document.getElementById("book-container");
+    bookContainer.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Year: ${book.year}</p>
+        `;
+        bookContainer.appendChild(bookCard);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", displayBook);
