3cf02da5ca83b33b439509e51f61efb724bf7f68
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-03-25 16:15:23 +0200

    script for submitting new books to library from form

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ function Book(title, author, year) {
     this.id = crypto.randomUUID();
 }
 
-const myLibrary = [
-    // new Book("The Hobbit", "J.R.R. Tolkein", 1937),
-    // new Book("To Kill A Mockingbird", "Harper Lee", 1960),
-    // new Book("1984", "George Orwell", 1949)
-];
+const myLibrary = [];
 
-function addBookToLibrary(title, author, year) {
-    const newBook = new Book(`${this.title} by ${this.author}, ${this.year}`);
-    myLibrary.push(newBook);
-}
+// function addBookToLibrary(title, author, year) {
+//     const newBook = new Book(`${this.title} by ${this.author}, ${this.year}`);
+//     myLibrary.push(newBook);
+// }
 
 function displayBook() {
     const bookContainer = document.getElementById("book-container");
@@ -39,6 +35,22 @@ button.addEventListener('click', () => {
     formDisplay.style.display = "grid";
 }) 
 
+const bookForm = document.getElementById("book-form");
 
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const year = document.getElementById("year").value;
+    const book = {
+        title: title,
+        author: author,
+        year: year,
+    }
+    myLibrary.push(book);
+    displayBook();
+    bookForm.reset();
+    bookForm.style.display = "none";
+});
 
 // document.addEventListener("DOMContentLoaded", displayBook);
